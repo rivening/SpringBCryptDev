df0cfd72ff6cf0d068c48230adaff301e423e452
리브닝 <riveningk@gmail.com>
2023-11-30 13:39:52 +0900

    springBcrypt

--- src/main/java/com/bcrypt/springbcryptdev/SpringBCryptDevApplication.java
+++ src/main/java/com/bcrypt/springbcryptdev/SpringBCryptDevApplication.java
@@ -1,13 +1,28 @@
 package com.bcrypt.springbcryptdev;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SpringBCryptDevApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBCryptDevApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // BCryptPasswordEncoder 인스턴스 생성
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        // 패스워드 해싱
+        String rawPassword = "user123";
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+
+        // 결과 출력
+        System.out.println("Original Password: " + rawPassword);
+        System.out.println("Encoded Password: " + encodedPassword);
+
+        // 패스워드 일치 여부 확인
+        boolean isMatch = passwordEncoder.matches(rawPassword, encodedPassword);
+        System.out.println("Password Matches: " + isMatch);
+
+    }
 
 }
--- src/test/java/com/bcrypt/springbcryptdev/SpringBCryptDevApplicationTests.java
+++ src/test/java/com/bcrypt/springbcryptdev/SpringBCryptDevApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBCryptDevApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
